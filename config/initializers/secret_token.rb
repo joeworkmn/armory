d03d83a0dce238eb5858b1abaf3a65ffe2504e97
Joseph Workman <joeworkmn@gmail.com>
2013-10-06 17:16:12 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Armory::Application.config.secret_key_base = '635b185bfe7390b5d9db9553cfb673c8aa7af68f08b52cb23bdb8ab6d324ef16d55a9fddce02d605bf2c18884be53f141f8259eb3e6196bdfc5c918bca62956d'
