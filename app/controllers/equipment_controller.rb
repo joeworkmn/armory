748f22914ee243ce1edc7c3d736c7a3e7d73c012
Joseph Workman <joeworkmn@gmail.com>
2013-10-07 01:14:18 -1000

    Able to create equipment

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -4,5 +4,22 @@ class EquipmentController < ApplicationController
    end
 
    def create
+      @equipment = Equipment.new(equipment_params)
+      if @equipment.save
+         flash[:success] = "Item has been created."
+         redirect_to new_equipment_path
+      else
+         render :new
+      end
+   end
+
+
+
+private
+
+   def equipment_params
+      params.require(:equipment)
+            .permit(:name, :strength, :agility, :stamina, :intellect, 
+                    :spirit, :attack_power, :critical, :dodge)
    end
 end
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: equipment
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  strength     :integer          default(0)
+#  agility      :integer          default(0)
+#  stamina      :integer          default(0)
+#  intellect    :integer          default(0)
+#  spirit       :integer          default(0)
+#  attack_power :integer          default(0)
+#  critical     :integer          default(0)
+#  dodge        :integer          default(0)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Equipment < ActiveRecord::Base
+   validates :name, presence: true
+   validates_numericality_of :strength, :agility, :stamina, :intellect, 
+                             :spirit, :attack_power, :critical, :dodge
+
 end
