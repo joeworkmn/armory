1d9cef189df3f8c2dd8a0018dc9e18af8061ca44
Joseph Workman <joeworkmn@gmail.com>
2013-10-12 22:40:02 -1000

    Able to make stat comparisons

--- app/assets/javascripts/equipment/index.js
+++ app/assets/javascripts/equipment/index.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
+
+   // Page Elements
+   firstItem = ".first-item"
+   secondItem = ".second-item"
+   compareLink = ".compare-link"
+
+
+
+
    initDataTable()
 
-   $(".data-table-gem").on('click', '.compare-link', function(e) {
+   $(".data-table-gem").on('click', compareLink, function(e) {
       e.preventDefault()
 
       equipment = $(this).data("attrs")
@@ -10,13 +19,46 @@ $(document).ready(function() {
       html = "<h4>" + equipment.name + "</h4>"
 
       $.each(stats, function(k, v) {
-         html += v + ": " + equipment[v] + "<br /> <br />"
+         html += "<span class='" + v + "'>" + v + ": " + equipment[v] + "<span class='comparison'></span> </span> <br /> <br />"
       })
 
-      if ( $(".first-item").is(":empty") ) {
-         $(".first-item").html(html)
+      if ( $(firstItem).is(":empty") ) {
+         $(firstItem).html(html)
+         $(firstItem).data("item", JSON.stringify(equipment))
       } else {
-         $(".second-item").html(html)
+         $(secondItem).html(html)
+         $(secondItem).data("item", JSON.stringify(equipment))
+      }
+
+
+      if (isReadyToCompare()) {
+         item1 = JSON.parse(getComparedItem(firstItem))
+         item2 = JSON.parse(getComparedItem(secondItem))
+
+         firstItemResult = item1.strength - item2.strength
+
+         secondItemResult = (firstItemResult > 0) ? "-" + firstItemResult : firstItemResult * -1
+
+         $(firstItem + " .strength .comparison").html(firstItemResult)
+         $(secondItem + " .strength .comparison").html(secondItemResult)
+
+
       }
    })
+
+
+
+
 })
+
+function isReadyToCompare() {
+   if ( $.trim(getComparedItem(firstItem)) != "" &&  $.trim(getComparedItem(secondItem)) != "" ) {
+      return true
+   } else {
+      return false
+   }
+}
+
+function getComparedItem(item) {
+   return $(item).data("item")
+}
